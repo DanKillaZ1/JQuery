39c23600b2434dc601476300d0124d8f057cbd48
DanKillaz <nielmarcos@hotmail.com>
2022-08-20 20:45:10 -0300

    finalizando Jquery

--- .js/custom.js
+++ .js/custom.js
@@ -236,6 +236,28 @@ function validaCpf(elemento){
       
    })
 }
+
+function validaEmail (elemento){
+   elemento.addEventListener('focusout', function(event){
+      event.preventDefault();
+
+      if(this.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+         document.querySelector('.mensagem').innerHTML = "",
+         this.classList.remove('erro');
+         this.parentNode.classList.remove('erro');
+      }else{
+         document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em vermelho";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+      }
+      
+   })
+}
+
+
+
+
 let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
 let camposCpf = document.querySelectorAll('input.cpf');
 
@@ -248,3 +270,8 @@ for ( let emFoco of camposObrigatorios){
 for ( let emFoco of camposCpf){
    validaCpf(emFoco);
 }
+
+for ( let emFoco of camposEmail){
+   validaEmail(emFoco);
+}
+
